d74b2e1cabfb06c2f29e0d5be87b4effd07311fd
Атанас Станчев <mr_Stanchev@hotmail.com>
2019-08-26 16:55:56 +0300

    Add 2, 3 and 4

---
+++ Chapter 2/greetingByName.js
@@ -0,0 +1,4 @@
+function sayHello([arg1]) {
+    var name = arg1;
+    console.log(`Hello, ${name}!`);
+    }
---
+++ Chapter 2/inchesToCentimeters.js
@@ -0,0 +1,5 @@
+function convertInchesToCentimeters([arg1]) {
+    var inches = parseFloat(arg1);
+    var centimeters = inches * 2.54;
+    console.log('Centimeters = ' + centimeters);
+    }
